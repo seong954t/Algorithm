f71f4b02a44eeb6be89267936b597f47b2a9618a
kde0820 <kde0820@naver.com>
2019-03-04 15:14:05 +0900

    random pick by python

---
+++ 당첨자 선정/김다은/random_pick.py
@@ -0,0 +1,13 @@
+import random
+
+people_list = ["김기환", "김다은", "김미지", "김수연", "김승태", "김진혁", "나재성", "신승혁", "장은정", "최소영"]
+prev_list = ["김다은", "김승태", "김수연", "김진혁", "최소영"]
+pick_num = 2
+pick_list = []
+
+while len(pick_list) < pick_num:
+    random_pick = random.choice(people_list)
+    if random_pick not in prev_list and random_pick not in pick_list:
+        pick_list.append(random_pick)
+
+print(pick_list)
