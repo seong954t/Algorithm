9d125abd8b3a5b0a0762b160e7853bd8925e2442
miiiji <alwldmsrk@gmail.com>
2019-03-08 22:39:00 +0900

    maze done

--- 1226. 미로1/김미지/Main.java
+++ 1226. 미로1/김미지/Main.java
@@ -1,60 +1,56 @@
 package com.company;
 
+import java.io.*;
+import java.util.Scanner;
 import java.util.*;
-
-
 public class Main {
+    private static int arr[][];
+    private static boolean visited[][];
+    private static int EXIT = 3;
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         // write your code here
-        Scanner sc = new Scanner(System.in);
+        boolean result;
+        int test = 1;
+        int start = 0;
+        int end = 0;
 
-        int test_num = sc.nextInt();
-        int line = 1;
-        int n,m,f;
-        while(line <= test_num){
-            n = sc.nextInt();
-            int[] for_mapping = new int[1000000];
-
-            int max = -1;
-            for (int i = 0 ; i<n; i++){
-                m = sc.nextInt();
-                f = sc.nextInt();
-                for_mapping[m] = f;
-                if(max < m){
-                    max = m;
+        while (test <= 10) {
+            br.readLine();
+            arr = new int[16][16];
+            visited = new boolean[16][16];
+            for (int i = 0; i < 16; i++) {
+                String[] temp = br.readLine().split("");
+                for (int j = 0; j < 16; j++) {
+                    arr[i][j] = Integer.parseInt(temp[j]);
+                    if(arr[i][j] == 2){
+                        start = i;
+                        end = j;
+                    }
                 }
             }
+            result = finding(start,end);
+            int temp = result ? 1 : 0;
+            System.out.println("#"+test+" "+temp);
+            test++;
+        }
+        br.close();
+    }
 
-            ArrayList<Integer> for_result;
-            ArrayList<Integer> clone_result = null;
-
-            for (int i = max; i>=0; i--){
-                if(for_mapping[i] != 0){
-
-                    int temp_int = i;
-                    for_result = new ArrayList<Integer>();
-
-                    while(for_mapping[temp_int]!=0){
-                        for_result.add(temp_int);
-                        for_result.add(for_mapping[temp_int]);
-                        temp_int = for_mapping[temp_int];
-                    }
-
-                    if(clone_result == null || for_result.size() > clone_result.size()){
-                        clone_result = (ArrayList<Integer>) for_result.clone();
-                    }
-
-                }
+    public static boolean finding(int start, int end) {
+        if(arr[start][end] == EXIT){
+            return true;
+        }
+        if(arr[start][end] == 1||visited[start][end] || start<0||end<0||start>=arr.length||end>=arr.length) {//출구를 못찾음(start,end가 음수값이 나오면 안됨,
+            return false;
+        }else {
+            visited[start][end] = true;
+            if (finding(start + 1, end) || finding(start - 1, end) || finding(start, end - 1) || finding(start, end + 1)) {//출구 찾아야행
+                return true;
             }
-
-            System.out.print("#" + line + " ");
-            for(int i = 0; i<clone_result.size(); i++){
-                System.out.print(clone_result.get(i) + " ");
-            }
-            System.out.println();
-            line++;
+            return false;
         }
     }
 }
