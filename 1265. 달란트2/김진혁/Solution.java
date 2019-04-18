f1931782a8c1304263fcb20207876b2fbe10b782
jinhyeok <persona5173@gmail.com>
2019-04-18 23:16:45 +0900

    [김진혁] modify 1265. 달란트2

--- 1265. 달란트2/김진혁/Solution.java
+++ 1265. 달란트2/김진혁/Solution.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -13,31 +14,24 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 		
 		for(int t = 1; t <= T; t++) {
-			int N = Integer.parseInt(br.readLine());
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int P = Integer.parseInt(st.nextToken());
 			
-			int[] busStop = new int[5001];
+			int x = N/P;
+			int y = N%P;
 			
-			for(int n = 0; n < N; n++) {
-				StringTokenizer st = new StringTokenizer(br.readLine());
-		
-				int i = Integer.parseInt(st.nextToken());
-				int j = Integer.parseInt(st.nextToken());
-				
-				while(i != j+1) {
-					busStop[i]++;
-					i++;
-				}
+			BigInteger ans = new BigInteger("1");
+			
+			for(int i = 0; i < P-y; i++) {
+				ans = ans.multiply(BigInteger.valueOf(x));
+			}
+			for(int i = 0; i < y; i++) {
+				ans = ans.multiply(BigInteger.valueOf(x+1));
 			}
 			
-			int P = Integer.parseInt(br.readLine());
-			
-			bw.write("#"+t + " ");
-			
-			for(int p = 0; p < P; p++) {
-				bw.write(busStop[Integer.parseInt(br.readLine())] + " ");
-			}
-			
-			bw.write("\n");
+
+			bw.write("#"+t + " " +ans+"\n");
 		}
 		
 		bw.flush();
